7858bb5a7285b8088d26a9342b8fa9e69eba8213
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-03-02 16:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+//we got the input in the inputField
+let inputField=document.getElementById('fname');
+
+// Add an event listener for the 'input' event
+inputField.addEventListener('input', function (event) {
+    // Get the current value of the input field
+    const inputValue = event.target.value;
+
+    // Convert the input value to uppercase
+    const uppercaseValue = inputValue.toUpperCase();
+
+    // Update the input field with the uppercase value
+    event.target.value = uppercaseValue;
+});
+
+//we update the eisitng string to uppercase
